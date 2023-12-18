aab4f2773a0ff66e28244ecbbcbfa8b758f5ef23
kang1211 <zdxsca1211@naver.com>
2023-12-18 20:26:05 +0900

    sss

---
+++ study/src/study121801/homeWork1218.java
@@ -0,0 +1,29 @@
+package study121801;
+
+public class homeWork1218 {
+	public static void main(String[] args) {
+		int[] score = {30, 4, 50, 60, 14, 80, 45, 72, 94, 25};
+		int avg = (30+4+50+60+14+80+45+72+94+25)/10;
+		String[] rating = new String[10];
+		int k=0;
+		
+		for(int i=0;i<score.length;i++) {
+			if(score[i]<=avg) {
+				rating[k] = "F";
+				k++;
+			}else{
+				rating[k] = "A";
+				k++;
+			}
+			System.out.print(rating[i]);	
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
