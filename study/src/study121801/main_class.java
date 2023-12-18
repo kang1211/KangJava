8622cb52652adbb74e4ccbbce8c471b874712297
kang1211 <zdxsca1211@naver.com>
2023-12-18 10:27:52 +0900

    sssss

--- study/src/study121801/main_class.java
+++ study/src/study121801/main_class.java
@@ -2,7 +2,13 @@ package study121801;
 
 public class main_class {
 	public static void main(String[] args) {
-		System.out.println("가나다");
-		System.out.println("마바사");
+		
+		// 자바스크립트 - let a = new Array();
+		// 			   let b = [10,20,30,40];
+		//		a.push(3123); 	b.push(233);
+		
+		int[] arr1 = {101,20};
+		
+		System.out.println(arr1[0]);
 	}
 }
