58c476c9532f88f25b26302125e78ef88f32cd61
kang1211 <zdxsca1211@naver.com>
2023-12-20 09:37:37 +0900

    ssss

--- study/src/study122001/what_class.java
+++ study/src/study122001/what_class.java
@@ -2,6 +2,9 @@ package study122001;
 
 public class what_class {
 	public static void main(String[] args) {
-		
+		// 클래스란 - 사용자 정의 데이터 타입
+		// 			기존의 기본 데이터 타입이 아닌 새로운 형태의 타입을 직접 정의하는 것
+		//			기본 데이터 타입과의 차이점은, 여러종류의 데이터를 다룰수도 있으며
+		//			기능(메서드)을 정의 할 수 있다.
 	}
 }
