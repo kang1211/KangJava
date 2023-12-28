41f9d838d5a2d5e718670f0a1c3d6d85ee21942e
kang1211 <zdxsca1211@naver.com>
2023-12-28 17:34:49 +0900

    ㄴㄴㄴㄴㄴㄴ

--- notice/src/notice/information.java
+++ notice/src/notice/information.java
@@ -2,8 +2,8 @@ package notice;
 
 public class information {
 	public static void main(String[] args) {
-//		깃허브 토큰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
+//		깃허브 토큰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
 		
-		//dafafsf
+		//수정
 	}
 }
