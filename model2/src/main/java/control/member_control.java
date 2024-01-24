106eab77892bf1b87d140f9b3d85ecbc2f438e6c
kang1211 <zdxsca1211@naver.com>
2024-01-24 17:31:20 +0900

    aaaaaa

--- model2/src/main/java/control/member_control.java
+++ model2/src/main/java/control/member_control.java
@@ -1,6 +1,10 @@
 package control;
 
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.Servlet;
@@ -11,15 +15,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import service.member_service;
+import service.member_action;
 
 /**
  * Servlet implementation class member_control
  */
-@WebServlet("/members")
+@WebServlet("/member_control")
 public class member_control extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private member_service ms = new member_service();
+	private Map<String, member_action> target = new HashMap<>();
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -33,7 +37,21 @@ public class member_control extends HttpServlet {
 	 * @see Servlet#init(ServletConfig)
 	 */
 	public void init(ServletConfig config) throws ServletException {
-		// TODO Auto-generated method stub
+		ResourceBundle rb = ResourceBundle.getBundle("class_bundle/member");
+		Enumeration key = rb.getKeys();
+		while(key.hasMoreElements()) {
+			String k = (String)key.nextElement(); // signup, signupSave, myPage ...등등
+			String value = rb.getString(k);// member.properties의 키에 맞는 value 가져오기
+			try {
+				Class<?> hcl = Class.forName(value);//클래스를 로드(객체생성하려면 클래스가 메모리에 로드 되어야 한다.)
+				member_action hinst = (member_action)hcl.newInstance();//객체생성
+				target.put(k, hinst);
+				
+			}catch(Exception e) {
+				System.out.println("member.properties 파일 작업 실패");
+				e.printStackTrace();
+			}
+		}
 	}
 
 	/**
@@ -61,21 +79,14 @@ public class member_control extends HttpServlet {
 		// 서버 대신해서 요청을 처리해주는 역할 을 한다. 
 		request.setCharacterEncoding("UTF-8");
 		
+		String uri = request.getRequestURI();
 		
-		
-		String cmd = request.getParameter("cmd"); // 요청 주소의 cmd 파라미터 값 가져오기
+		String cmd = uri.substring(9); // 요청 주소의 cmd 파라미터 값 가져오기
 		String view ="/"; // 사용자에게 제공할 기본 페이지
 		
-		if (cmd.equals("signup")) {  // 요청 파라미터 가 signup일경우 회원가입페이지를 요청한거
-			request.setAttribute("prt", "member/signup");
-			request.setAttribute("emailList", ms.emailList());
-			// 회원가입 페이지를 제공하기위해 request객체를 이용한것일뿐 반드시 이와같은 
-			// 방법이 아니여도된다.
-			// 사용자 요청에 의한 페이지 제공은 정적페이지(.html, .jsp 등) 그대로 제공할수도있고
-			// 위와 같이 include 방식으로 제공 할 수 있다.
-		}else if( cmd.equals("signSave") ) {
-			view = ms.insert(request, response);
-		}
+		member_action mem = target.get(cmd);
+		
+		view = mem.action(request, response);
 		
 		if( view != null) {
 			RequestDispatcher rds = request.getRequestDispatcher(view);//제공할 페이지
@@ -87,3 +98,11 @@ public class member_control extends HttpServlet {
 	}
 
 }
+/*
+ * if (cmd.equals("signup")) { // 요청 파라미터 가 signup일경우 회원가입페이지를 요청한거
+ * request.setAttribute("prt", "member/signup");
+ * request.setAttribute("emailList", ms.emailList()); // 회원가입 페이지를 제공하기위해
+ * request객체를 이용한것일뿐 반드시 이와같은 // 방법이 아니여도된다. // 사용자 요청에 의한 페이지 제공은 정적페이지(.html,
+ * .jsp 등) 그대로 제공할수도있고 // 위와 같이 include 방식으로 제공 할 수 있다. }else if(
+ * cmd.equals("signSave") ) { view = ms.insert(request, response); }
+ */
